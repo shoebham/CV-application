8b2355b4a2763d9ca591866319c3dfa8e711b876
Shubham <25881429+shoebham@users.noreply.github.com>
2022-04-16 22:32:29 +0530

    init skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <div>
+        <input type="text" placeholder='First Name'/>
+        <input type="text" placeholder='Last Name'/>
+        <input type="number" placeholder='Contact number'/>
+        <input type="email" placeholder='Email'/>
+        <input type="text" placeholder='Address'/>
+        <input type="text" placeholder='City'/>
+        <input type="text" placeholder='State'/>
+        <input type="text" placeholder='Zip Code'/>
+        <input type="text" placeholder='Country'/>
+        {/* work experience details */}
+        <input type="text" placeholder='Company Name'/>
+        <input type="text" placeholder='Job Title'/>
+        <input type="text" placeholder='Job Description'/>
+        <input type="text" placeholder='Job Start Date'/>
+        <input type="text" placeholder='Job End Date'/>
+        <input type="text" placeholder='Job Location'/>
+        {/* Education details */}
+        <input type="text" placeholder='School Name'/>
+        <input type="text" placeholder='School Location'/>
+        <input type="text" placeholder='Degree'/>
+        <input type="text" placeholder='Major'/> 
+        <input type="text" placeholder='GPA'/>
+        
+      </div>
+    )
+  }
 }
-
 export default App;
